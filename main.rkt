ac4d481467546d4845085883205effd5919dfd06
Kevin Robert Stravers <macocio@gmail.com>
2017-01-12 08:00:42 +0100

    Implement the button-corrector algorithm

--- main.rkt
+++ main.rkt
@@ -1,13 +1,30 @@
 #! /usr/bin/env racket
 #lang racket
 
+[require threading] ; This is for the ~> and ~>> operators, and has nothing to do with parallel threads
+
+;; Useful functions
+[define (not-empty list)
+  [not [empty? list]]]
+[define (showpipe . rest)
+  [displayln rest]
+  [last rest]]
+
 ;; The state each elevator contains
 [struct state (position waypoints external-buttons internal-buttons resting-position) #:prefab]
 
 ;; Ensure all elevators have the same external-buttons state
 [define (correct-buttons all-known-elevator-states)
-  [let ([s all-known-elevator-states])
-    [error "Function not yet implemented"]]]
+  ;; Merge all lists into a single list
+  [define (merge-lists list-of-lists)
+    [apply append list-of-lists]]
+  ;; Remove all numeric duplicates in O(n^2) time
+  [define (remove-duplicates list)
+    [foldr [lambda [number state] [cons number [filter [lambda [z] [not [= number z]]] state]]] empty list]]
+  [~>>
+    [map state-external-buttons all-known-elevator-states]
+    merge-lists
+    remove-duplicates]]
 
 ;; Remove floors (buttons) which we can confirm have been cleared by an elevator
 [define (prune-buttons all-known-elevator-states)
@@ -18,3 +35,12 @@
 [define (assign-mission all-known-elevator-states
   [let ([s all-known-elevator-states])
     [error "Function not yet implemented"]]]
+
+;; Example
+[define example-states [list
+                         [state 0 '[] '[1 3] '[] 0]
+                         [state 0 '[] '[] '[] 0]
+                         [state 0 '[] '[9] '[] 0]
+                         [state 0 '[] '[9 8 8 8 3] '[] 0]
+                         [state 5 '[] '[1]   '[] 5]]]
+[correct-buttons example-states]
