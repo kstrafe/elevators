2aa4d0ace86bc6498c6ff9f23185e946cef0f6a3
Kevin Robert Stravers <macocio@gmail.com>
2017-01-31 21:50:39 +0100

    Use udp-receive!* instead of async channels

--- Project/network/network.rkt
+++ Project/network/network.rkt
@@ -16,19 +16,12 @@
 [define udp-channel [udp-open-socket]]
 [udp-bind! udp-channel #f broadcast-port]
 
-[define channel [make-async-channel 5000]]
-
-[define queuer (thread (lambda ()
-  [let loop ([input-buffer [make-bytes 1024]])
-    [let-values ([(message-length source source-port) [udp-receive! udp-channel input-buffer]])
-      [async-channel-put channel [subbytes input-buffer 0 message-length]]
-      [loop input-buffer]]]))]
-
 [define (receive)
-  [let ([received-value [async-channel-try-get channel]])
-    [if received-value
-      [cons [fasl->s-exp received-value] [receive]]
-      empty]]]
+  [let ([input-buffer [make-bytes 1024]])
+    [let-values ([(message-length source-host source-port) [udp-receive!* udp-channel input-buffer]])
+      [if message-length
+        [cons [fasl->s-exp input-buffer] [receive]]
+        empty]]]]
 
 [define (send info)
   [udp-send-to udp-channel broadcast broadcast-port [s-exp->fasl info]]]
