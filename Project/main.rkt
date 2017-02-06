c0457c0c85d774fc908b96a549af27df4bcfac4b
Dag Erik <dagerikhl@gmail.com>
2017-02-06 18:29:25 +0000

    Simplifying filehandling-system for neater code

--- Project/main.rkt
+++ Project/main.rkt
@@ -4,27 +4,20 @@
 (require libuuid
          "name-generator.rkt")
 
-(define (write-identity filepath)
-  (let ([id (uuid-generate)]
-        [name (generate-name)])
-    (with-handlers ([exn:fail:filesystem?
-                      (lambda (e)
-                        (displayln "file not writable"))])
-      (with-output-to-file filepath
-        (lambda () (write (list id name)))))
-    (values id name)))
-
 (define (generate-identity)
-  (let ([filepath "_elevator-uuid"])
-    (if (file-exists? filepath)
-      (with-handlers ([exn:fail:filesystem?
-                        (lambda (e)
-                          (displayln "file not readable")
-                          (delete-file filepath)
-                          (write-identity filepath))])
+  (with-handlers ([exn:fail:filesystem?
+                    (lambda (e)
+                      (displayln "filesystem error")
+                      (values (uuid-generate) (generate-name)))])
+    (let ([filepath "_elevator-uuid"])
+      (if (file-exists? filepath)
         (with-input-from-file filepath
-          (lambda () (apply values (file->value filepath)))))
-      (write-identity filepath))))
+          (lambda () (apply values (file->value filepath))))
+        (let ([id (uuid-generate)]
+              [name (generate-name)])
+          (with-output-to-file filepath
+            (lambda () (write (list id name))))
+          (values id name))))))
 
 (define-values (id name) (generate-identity))
 
