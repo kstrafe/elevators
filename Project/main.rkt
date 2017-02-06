74576f61ee66923196218b6f64d80e1362ef2ed5
Dag Erik <dagerikhl@gmail.com>
2017-02-06 00:40:35 +0000

    Adding file-storing of uuid and name

--- Project/main.rkt
+++ Project/main.rkt
@@ -4,7 +4,25 @@
 (require libuuid
          "name-generator.rkt")
 
-(define-values (id name) (values (uuid-generate) (generate-name)))
+(define (generate-identity)
+  (let ([filepath "_elevator-uuid"])
+    (if (file-exists? filepath)
+      (let* ([in (open-input-file filepath)]
+             [id (read-line in 'any)]
+             [name (read-line in 'any)])
+        (close-input-port in)
+        (values id name))
+      (let* ([id (uuid-generate)]
+             [name (generate-name)]
+             [out (open-output-file filepath)])
+        (write-string id out)
+        (newline out)
+        (write-string name out)
+        (newline out)
+        (close-output-port out)
+        (values id name)))))
+
+(define-values (id name) (generate-identity))
 
 ;; DEBUG
 id
