0d98109e8b6173ebd6285ffe65e48ba8f380c412
Dag Erik <dagerikhl@gmail.com>
2017-02-06 12:58:58 +0000

    Delegating writing to own function and deleting file

--- Project/main.rkt
+++ Project/main.rkt
@@ -4,20 +4,24 @@
 (require libuuid
          "name-generator.rkt")
 
+(define (write-identity filepath)
+  (let ([id (uuid-generate)]
+        [name (generate-name)])
+    (with-output-to-file filepath
+      (lambda () (write (list id name) )))
+    (values id name)))
+
 (define (generate-identity)
   (let ([filepath "_elevator-uuid"])
     (if (file-exists? filepath)
       (with-handlers ([exn:fail:filesystem?
                         (lambda (e)
                           (displayln "file not readable")
-                          (values (uuid-generate) (generate-name)))])
+                          (delete-file filepath)
+                          (write-identity filepath))])
         (with-input-from-file filepath
           (lambda () (apply values (file->value filepath)))))
-      (let ([id (uuid-generate)]
-            [name (generate-name)])
-        (with-output-to-file filepath
-          (lambda () (write (list id name) )))
-        (values id name)))))
+      (write-identity filepath))))
 
 (define-values (id name) (generate-identity))
 
