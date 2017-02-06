e4794bdf2346186de451a80a92329f88bea95128
Dag Erik <dagerikhl@gmail.com>
2017-02-06 11:49:32 +0000

    Add exception-handling of permission denied to uuid-file

--- Project/main.rkt
+++ Project/main.rkt
@@ -6,14 +6,18 @@
 
 (define (generate-identity)
   (let ([filepath "_elevator-uuid"])
-    (if (file-exists? filepath)
-      (with-input-from-file filepath
-        (lambda () (apply values (file->lines filepath))))
-      (let ([id (uuid-generate)]
-            [name (generate-name)])
-        (with-output-to-file filepath
-          (lambda () (printf (string-join (list id name) "\n"))))
-        (values id name)))))
+    (with-handlers ([exn:fail:filesystem?
+                      (lambda (e)
+                        (displayln "got a filesystem error")
+                        (values (uuid-generate) (generate-name)))])
+      (if (file-exists? filepath)
+        (with-input-from-file filepath
+          (lambda () (apply values (file->lines filepath))))
+        (let ([id (uuid-generate)]
+              [name (generate-name)])
+          (with-output-to-file filepath
+            (lambda () (printf (string-join (list id name) "\n"))))
+          (values id name))))))
 
 (define-values (id name) (generate-identity))
 
