36694a1a0ba7ac801e3ce1b327f130f7d609d866
Dag Erik <dagerikhl@gmail.com>
2017-02-06 10:31:02 +0000

    File input/output now uses with-...-file to read/write

--- Project/main.rkt
+++ Project/main.rkt
@@ -7,19 +7,12 @@
 (define (generate-identity)
   (let ([filepath "_elevator-uuid"])
     (if (file-exists? filepath)
-      (let* ([in (open-input-file filepath)]
-             [id (read-line in 'any)]
-             [name (read-line in 'any)])
-        (close-input-port in)
-        (values id name))
-      (let* ([id (uuid-generate)]
-             [name (generate-name)]
-             [out (open-output-file filepath)])
-        (write-string id out)
-        (newline out)
-        (write-string name out)
-        (newline out)
-        (close-output-port out)
+      (with-input-from-file filepath
+        (lambda () (apply values (file->lines filepath))))
+      (let ([id (uuid-generate)]
+            [name (generate-name)])
+        (with-output-to-file filepath
+          (lambda () (printf (string-join (list id name) "\n"))))
         (values id name)))))
 
 (define-values (id name) (generate-identity))
