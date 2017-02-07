15fed174e9c1449163a7c9bcc4667e659aa4364c
Kevin Robert Stravers <macocio@gmail.com>
2017-02-07 12:47:48 +0100

    Implement async sending of information

--- Project/network/network.rkt
+++ Project/network/network.rkt
@@ -7,14 +7,16 @@
          racket/serialize
          racket/fasl]
 
+(define broadcast-sleep 0.4)
 [define broadcast-port 30073]
-[define broadcast
+[define broadcast-address
   [let-values [([p o i e]
     [subprocess #f #f #f "/usr/bin/env" "bash" "-c" "ifconfig | grep Bcast | cut -d':' -f 3 | cut -d' ' -f 1"])]
     [read-line o]]]
 
 [define udp-channel [udp-open-socket]]
 [udp-bind! udp-channel #f broadcast-port]
+(define sender-channel (make-async-channel))
 
 [define (receive)
   [let ([input-buffer [make-bytes 1024]])
@@ -23,5 +25,16 @@
         [cons [fasl->s-exp input-buffer] [receive]]
         empty]]]]
 
+(define sender-thread (thread (lambda ()
+  (let loop ([to-send #f])
+    (let ([new-value (async-channel-try-get sender-channel)])
+      (if new-value
+        (loop new-value)
+        (begin
+          (when to-send
+            [udp-send-to udp-channel broadcast-address broadcast-port [s-exp->fasl to-send]])
+          (sleep broadcast-sleep)
+          (loop to-send))))))))
+
 [define (send info)
-  [udp-send-to udp-channel broadcast broadcast-port [s-exp->fasl info]]]
+  (async-channel-put sender-channel info)]
