2bcc5c1efd9768c6121f6b12916a619621815df1
Kevin Robert Stravers <macocio@gmail.com>
2017-02-07 12:54:00 +0100

    Change [] to () and reorder the code

--- Project/network/network.rkt
+++ Project/network/network.rkt
@@ -1,29 +1,32 @@
 #! /usr/bin/env racket
 #lang racket
 
-[provide receive send]
+(provide receive send)
 
-[require racket/async-channel
+(require racket/async-channel
          racket/serialize
-         racket/fasl]
+         racket/fasl)
 
+(define broadcast-address
+  (let-values ([(p o i e)
+    (subprocess #f #f #f "/usr/bin/env" "bash" "-c" "ifconfig | grep Bcast | cut -d':' -f 3 | cut -d' ' -f 1")])
+    (read-line o)))
+(define broadcast-port 30073)
 (define broadcast-sleep 0.4)
-[define broadcast-port 30073]
-[define broadcast-address
-  [let-values [([p o i e]
-    [subprocess #f #f #f "/usr/bin/env" "bash" "-c" "ifconfig | grep Bcast | cut -d':' -f 3 | cut -d' ' -f 1"])]
-    [read-line o]]]
 
-[define udp-channel [udp-open-socket]]
-[udp-bind! udp-channel #f broadcast-port]
 (define sender-channel (make-async-channel))
+(define udp-channel (udp-open-socket))
+(udp-bind! udp-channel #f broadcast-port)
 
-[define (receive)
-  [let ([input-buffer [make-bytes 1024]])
-    [let-values ([(message-length source-host source-port) [udp-receive!* udp-channel input-buffer]])
-      [if message-length
-        [cons [fasl->s-exp input-buffer] [receive]]
-        empty]]]]
+(define (receive)
+  (let ([input-buffer (make-bytes 1024)])
+    (let-values ([(message-length source-host source-port) (udp-receive!* udp-channel input-buffer)])
+      (if message-length
+        (cons (fasl->s-exp input-buffer) (receive))
+        empty))))
+
+(define (send info)
+  (async-channel-put sender-channel info))
 
 (define sender-thread (thread (lambda ()
   (let loop ([to-send #f])
@@ -32,9 +35,7 @@
         (loop new-value)
         (begin
           (when to-send
-            [udp-send-to udp-channel broadcast-address broadcast-port [s-exp->fasl to-send]])
+            (udp-send-to udp-channel broadcast-address broadcast-port (s-exp->fasl to-send)))
           (sleep broadcast-sleep)
           (loop to-send))))))))
 
-[define (send info)
-  (async-channel-put sender-channel info)]
