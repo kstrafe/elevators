0e8843d3b48d408a6d46a22732fbe3048a035551
Kevin Robert Stravers <macocio@gmail.com>
2017-02-07 12:48:49 +0100

    Implement part of the main loop

--- Project/main.rkt
+++ Project/main.rkt
@@ -1,13 +1,21 @@
 #! /usr/bin/env racket
 #lang racket
 
-(require "identity-generator.rkt"
+(require racket/pretty
+         "identity-generator.rkt"
          "network/network.rkt")
+
+;; Ensure that we use the incremental garbage collector
+(collect-garbage 'incremental)
 
 (define-values (id name) (generate-identity))
 
-(let loop ()
-  (send (string-append "Hi! My name is " name ", I'm also known as " id))
-  (displayln (receive))
-  (sleep 2)
-  (loop))
+(let loop ([my-friends empty])
+  (send `((string-append "Hi! My name is " ,name ", I'm also known as " ,id) ,id ,name ,(current-inexact-milliseconds)))
+  (pretty-print
+    (for/list ([message (receive)])
+      (match message
+        [(list _ id name time) (string-append "Yaaayy! Welcome " name " of " id " t=" (number->string time))]
+        [_ (displayln "Unknown message :( I'm scared")])))
+  (sleep 1)
+  (loop empty))
