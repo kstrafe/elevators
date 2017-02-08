314ca584eb69012fb7f71444f84842adb3d6c3af
Kevin Robert Stravers <macocio@gmail.com>
2017-02-08 05:11:15 +0100

    Implement hash-remove-predicate, hash-set-from-list, and map-hash-table
    These functions make updating the entire hash table easier.

--- Project/main.rkt
+++ Project/main.rkt
@@ -16,19 +16,45 @@
 (define-values (id name) (generate-identity))
 
 (define state (elevator-state id name 0))
+(define time-to-live 3)
 
 (dbug id name)
 
-(let loop ([my-friends (make-immutable-hash `([,id . ,(elevator-attributes state 3 (current-inexact-milliseconds))]))])
+;; Map each value in a hash-table (hash fn . -> . hash)
+(define (map-hash-table hash function)
+  (foldl (lambda (x s) (hash-set s x (function (hash-ref s x)))) hash (hash-keys hash)))
+
+(define-syntax hash-set-from-list
+  (syntax-rules (in: update-with:)
+    [(_ hash accessor in: list update-with: function)
+      (foldl (lambda (x s) (hash-set s (accessor x) (function x)))
+        hash
+        list)]))
+
+(define (hash-remove-predicate hash predicate)
+  (foldl (lambda (x s)
+    (if (predicate (hash-ref s x))
+      (hash-remove s x)
+      s))
+    hash
+    (hash-keys hash)))
+
+(let loop ([my-friends (make-immutable-hash `([,id . ,(elevator-attributes state time-to-live (current-inexact-milliseconds))]))])
   (send state)
   (sleep 1)
   (let ([messages (receive)])
-    (trce messages)
+    ; (trce messages)
+    (trce my-friends)
     (~>
-      (foldl (lambda (x s) (hash-set s x (lens-transform elevator-attributes-time-to-live-lens (hash-ref s x) sub1)))
-             my-friends
-             (hash-keys my-friends))
-      (foldl (lambda (x s) (hash-set s (elevator-state-id x) (elevator-attributes x 3 (current-inexact-milliseconds))))
-             _
-             messages)
+      ;; Decrement all 'time-to-live's
+      (map-hash-table my-friends (lambda (x)
+        (lens-transform elevator-attributes-time-to-live-lens x sub1)))
+
+      ;; Reset 'time-to-live' for the elevators from which we received a message
+      (hash-set-from-list _ elevator-state-id in: messages update-with: (lambda (msg)
+        (elevator-attributes msg time-to-live (current-inexact-milliseconds))))
+
+      ;; Remove all elevators where time-to-live <= 0
+      (hash-remove-predicate (lambda (x) (<= (elevator-attributes-time-to-live x) 0)))
+
       (loop))))
