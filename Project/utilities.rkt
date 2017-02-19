18c1925555e08d3c96390abc7c1233d4ef109824
Dag Erik <dagerikhl@gmail.com>
2017-02-19 20:10:23 +0100

    Remove with-input-from-file for non-parameterized streams

--- Project/utilities.rkt
+++ Project/utilities.rkt
@@ -12,11 +12,7 @@
 ;; Read commands from file
 (define (read-commands)
   (with-handlers ([exn? (lambda (e) (displayln e) empty)])
-    (let ([filepath "commands"])
-      (if (file-exists? filepath)
-        (with-input-from-file filepath
-          (lambda () (file->value filepath)))
-        empty))))
+    (let ([filepath "commands"]) (if (file-exists? filepath) (file->value filepath) empty))))
 
 ;; Get the first of a list or return empty
 (define (first-or-empty list) (if (empty? list) empty (first list)))
