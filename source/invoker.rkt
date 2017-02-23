403ca6162fe714399c7f1568ba20a901ab98b87d
Dag Erik <dagerikhl@gmail.com>
2017-02-23 22:33:37 +0100

    Move monitor-logit to invoker and use fifo pipe

--- source/invoker.rkt
+++ source/invoker.rkt
@@ -2,7 +2,16 @@
 
 (provide invoker)
 
-(require "core-unit.rkt" "identity-generator-unit.rkt" "utilities-unit.rkt" "logger.rkt")
+(require "core-unit.rkt" "data-structures.rkt" "identity-generator-unit.rkt" "logger.rkt" "monitor.rkt" "utilities-unit.rkt")
+
+;; Spawn a monitor that brings this elevator back online in case of failure
+(define-values (monitor monitor-out monitor-in monitor-err)
+  (subprocess #f #f #f "/usr/bin/env" "racket" "source/monitor.rkt"))
+(let ([error (port->string monitor-err)])
+  (when (non-empty-string? error) (warn "Unable to spawn monitor" error)))
+
+;; Create a pipe to use for communication between elevator and monitor
+(system* "/usr/bin/env" "bash" "mkfifo" "monitor-fifo")
 
 ;; Propagate invocation of the main function to core unit
 (define (invoker complex-struct)
@@ -10,4 +19,15 @@
   (define-values/invoke-unit/infer identity-generator@)
   (define-values/invoke-unit/infer utilities@)
   (define-values/invoke-unit/infer core@)
+
+  (when (not (empty? complex-struct))
+    ; (dbug (first (complex-commands complex-struct)))
+    ; (write (first (complex-commands complex-struct)) monitor-pipe-out))
+    (with-output-to-file "monitor-fifo"
+      (write (first (complex-commands complex-struct)))))
+
+  (if (not (= monitor-out #f))
+    (info (read monitor-out))
+    empty)
+
   (core complex-struct))
--- source/monitor.rkt
+++ source/monitor.rkt
@@ -1,9 +1,8 @@
 #lang racket
 
+(require "logger.rkt")
+
 (let loop ()
-  (if #t
-    ; (displayln (port->string))
-    ;; TODO This causes the entire program to freeze, figure out how to read from the input port
-    (port->string)
-    (displayln '#f))
+  ; (with-input-from-file "monitor-fifo"
+  ;   (dbug* (read)))
   (loop))
