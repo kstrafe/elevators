8b81743d1e88d58ac54292c2829b8cb8c3e696bf
Dag Erik <dagerikhl@gmail.com>
2017-03-06 22:33:54 +0100

    Add -9 to pkill for monitor to have smite power

--- source/monitor.rkt
+++ source/monitor.rkt
@@ -10,9 +10,8 @@
     (open-input-file "temporaries/monitor-fifo")))
 
 ;; Start a new main with backup commands as command line arguments
-(define (respawn-elevator message kill-old-monitor)
-  (when kill-old-monitor
-    (system* "/usr/bin/env" "pkill" "main.rkt"))
+(define (respawn-elevator message)
+  (system* "/usr/bin/env" "pkill" "-9" "-f" "main.rkt")
   (warn "You can't stump the Trump")
   (subprocess (current-output-port) (current-input-port) (current-error-port) "/usr/bin/env" "bash" "-c"
     (string-join (list "racket main.rkt" (string-append "\"" (~a message) "\"")))))
@@ -24,21 +23,20 @@
 (define fifo-reader (thread (lambda ()
   (let loop () (async-channel-put fifo-channel (read monitor-fifo-in)) (loop)))))
 
-;; Main lopp of the monitor.
+;; Main loop of the monitor.
 ;;
 ;; Tries to get a message read from fifo asynchronously.
-;; If it receives an EOF-object a new elevator is spawned;
-;; if it cannot read a value from fifo, a time-to-live counter
+;; If it cannot read a value from fifo, a time-to-live counter
 ;; is decremented on the next loop.
-;; When time-to-live becomes negative the old elevator is killed
-;; and a new one is spawned.
+;; When time-to-live becomes negative or EOF is received any
+;; old elevator is killed and a new one is spawned.
 ;; Else the loop continues with its new message.
 (let loop ([message empty] [time-to-live initial-time-to-live])
+  ; (trce time-to-live)
   (let ([message* (async-channel-try-get-last#io fifo-channel #f)]
         [time-to-live* (sub1 time-to-live)])
     (sleep 0.1)
     (cond
-      ([negative? time-to-live]  (respawn-elevator message #t))
-      ([eof-object? message*]    (respawn-elevator message #f))
-      ([false? message*]         (loop message time-to-live*))
-      (else                      (loop message* initial-time-to-live)))))
+      ([or (negative? time-to-live) (eof-object? message*)] (respawn-elevator message))
+      ([false? message*] (loop message time-to-live*))
+      (else (loop message* initial-time-to-live)))))
