62a8df9544b9efcde00f72511657824530326b70
Kevin Robert Stravers <macocio@gmail.com>
2017-03-16 09:19:12 +0100

    Add install script

---
+++ resources/install.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+echo "Password: "
+read pass
+if ! [ -e racket-6.8-x86_64-linux.sh ]; then
+	wget 'https://mirror.racket-lang.org/installers/6.8/racket-6.8-x86_64-linux.sh'
+fi
+chmod u+x racket-6.8*
+echo $pass | sudo -S ./racket-6.8*
+sudo raco pkg install --skip-installed lens libuuid reloadable sha threading <<< a
